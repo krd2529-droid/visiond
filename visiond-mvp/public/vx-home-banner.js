601a8650c74c7b6f9b01624854ee27c27458d3db
krd2529-droid <krd2529@gmail.com>
2026-09-06 12:45:43 +0700

    refactor(home): remove VX promotional GIF

--- visiond-mvp/public/vx-home-banner.js
+++
@@ -1,5 +0,0 @@
-(()=>{
-  const banner=document.querySelector('#vtools.vx-home-banner');if(!banner)return;
-  const promo=document.querySelector('[data-visiond-promo="bundle-discount"]');
-  if(promo)promo.before(banner);
-})();
